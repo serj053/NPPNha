ca42d598e1b8ed953e657209a6cd5fa7ba66f1e0
serj053 <serj053@yandex.ru>
2024-12-13 21:40:49 +0300

    init

---
+++ src/main/java/mainMenu/mainController.java
@@ -0,0 +1,23 @@
+package mainMenu;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class mainController {
+
+    public void itemNewTask(ActionEvent actionEvent) {
+    }
+
+    public void itemOpenTask(ActionEvent actionEvent) {
+    }
+
+    public void itemSaveTask(ActionEvent actionEvent) {
+    }
+
+    public void itemExportToCRM(ActionEvent actionEvent) {
+    }
+
+    public void itemExportRequest(ActionEvent actionEvent) {
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module org.example.nppnha {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens mainMenu to javafx.fxml;
+    exports mainMenu;
+}
